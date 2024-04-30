2c06b39b98b6b7332250000f75c4034442232f48
AlessioBucheri <157613253+AlessioBucheri@users.noreply.github.com>
2024-04-30 17:42:49 +0200

    reset app

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./RecipeCard.css";
 
-const RecipeCard = ({ recipe }) => {
+const RecipeCard = () => {
   return (
     <div className='recipe--card'>
-      <h4 className='recipe--title'>{recipe.title}</h4>
-      <img className='recipe--image' src={recipe.image} alt={recipe.title} />
+      <h4 className='recipe--title'>ricetta</h4>
+      <img className='recipe--image' src='' alt='' />
     </div>
   );
 };
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,14 @@
 import React, { useState } from "react";
 
-function SearchBar({ setSearchIngredient, onSearch }) {
-  const [ingredient, setIngredient] = useState("");
-
-  const handleInputChange = (e) => {
-    setIngredient(e.target.value);
-  };
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    setSearchIngredient(ingredient);
-    onSearch();
-  };
-
+function SearchBar() {
   return (
     <div className='search--bar'>
       <input
         className='search--input'
         type='text'
         placeholder='Type here your ingredient!'
-        value={ingredient}
-        onChange={handleInputChange}
       />
-      <button className='search--button' onClick={handleSearch}>
-        Search
-      </button>
+      <button className='search--button'>Search</button>
     </div>
   );
 }
